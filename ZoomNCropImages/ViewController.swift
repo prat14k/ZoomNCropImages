f4608c824024ba18f9ed8ae4d45e620b97d0f4a1
Prateek Sharma <prateek13103718@gmail.com>
2017-12-27 19:15:12 +0530

    Crop n Save Feature added

--- ZoomNCropImages/ViewController.swift
+++ ZoomNCropImages/ViewController.swift
@@ -14,16 +14,17 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
        let imageV = UIImageView(frame: CGRect(x: 0, y: 0, width: self.scrollView.frame.size.width, height: self.scrollView.frame.size.width))
         imageV.image = UIImage(named: "placeholderImage")
         imageV.isUserInteractionEnabled = true
-        
+
         imageV.contentMode = .scaleAspectFill
-        
+
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(loadImage(_:)))
         tapGesture.numberOfTapsRequired = 1
         imageV.addGestureRecognizer(tapGesture)
-        
+
         return imageV
     }()
     
+//    var imageView : UIImageView!
     
     @objc func loadImage(_ sender : UIGestureRecognizer) {
         
@@ -61,7 +62,28 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
     }
     
     func centerImageViewInScrollView(){
+        var imageBounds = imageView.frame
+        let scrollBounds = scrollView.bounds
         
+        if imageBounds.size.width < scrollBounds.size.width {
+            imageBounds.origin.x = (scrollBounds.size.width - imageBounds.size.width) / 2
+        }
+        else{
+            imageBounds.origin.x = 0
+        }
+        
+        if imageBounds.size.height < scrollBounds.size.height {
+            imageBounds.origin.y = (scrollBounds.size.height - imageBounds.size.height) / 2
+        }
+        else{
+            imageBounds.origin.y = 0
+        }
+        
+        imageView.frame = imageBounds
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerImageViewInScrollView()
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
@@ -71,7 +93,19 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+//        addImageView()
+    }
+    
+    func addImageView() {
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.scrollView.frame.size.width, height: self.scrollView.frame.size.height))
+        imageView.image = UIImage(named: "placeholderImage")
+        imageView.isUserInteractionEnabled = true
         
+        imageView.contentMode = .scaleAspectFill
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(loadImage(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        imageView.addGestureRecognizer(tapGesture)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -81,6 +115,26 @@ class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerContr
     }
     
     @IBAction func cropImageAction(_ sender: UIButton) {
+        
+        UIGraphicsBeginImageContextWithOptions(scrollView.bounds.size, true, UIScreen.main.scale)
+        let offset = scrollView.contentOffset
+        
+        CGContext.translateBy(UIGraphicsGetCurrentContext()!)(x: -offset.x, y: -offset.y)
+//        CGContextTranslateCTM(UIGraphicsGetCurrentContext()!, -offset.x, -offset.y)
+        scrollView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+        
+        let alert = UIAlertController(title: "Image Saved", message: "The Image is saved successfully in the Photo Library. Message will destruct afetr 2 seconds automatically.", preferredStyle: .alert)
+        
+        self.present(alert, animated: true) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+                alert.dismiss(animated: true, completion: nil)
+            })
+        }
     }
     
 }
